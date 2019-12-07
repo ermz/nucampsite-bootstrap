809e4332141fe03d51aed8106ef61a2a35aa5e7d
Edson Ramirez <edsonrmz.dev@gmail.com>
2019-12-06 23:45:57 -0500

    finished task 3 of week 2 assignment

--- Desktop/NucampFolder/3-React/nucampsite/src/components/DirectoryComponent.js
+++ Desktop/NucampFolder/3-React/nucampsite/src/components/DirectoryComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent.js';
 
 class Directory extends Component {
     constructor(props) {
@@ -49,7 +50,7 @@ class Directory extends Component {
                 <div className="row">
                     {directory}
                 </div>
-                
+                <CampsiteInfo campsite={this.state.selectedCampsite} />
                 {/* <div className="row">
                     <div className="col-md-5 m-1">
                         {this.renderSelectedCampsite(this.state.selectedCampsite)}
